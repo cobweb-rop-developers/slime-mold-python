339bc36433b73637ecb72134904c1b94de4007a2
ipekakyol <66427281+ipekakyol@users.noreply.github.com>
2023-02-08 15:47:28 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,18 @@
+"""Constants used through the simulation."""
+
+BOUNDS_WIDTH: int = 700
+MAX_X: float = BOUNDS_WIDTH / 2
+MIN_X: float = -MAX_X
+VIEW_WIDTH: int = BOUNDS_WIDTH + 20
+
+BOUNDS_HEIGHT: int = 700
+MAX_Y: float = BOUNDS_HEIGHT / 2
+MIN_Y: float = -MAX_Y
+VIEW_HEIGHT: int = BOUNDS_HEIGHT + 20
+
+CELL_RADIUS: int = 8
+CELL_COUNT: int = 1
+CELL_SPEED: float = 10.0
+
+# CELL CONSTANTS
+ENERGY_DECREASE = 12
